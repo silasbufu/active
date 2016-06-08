36bf9c8700d18a0cff2e92c411c58c88eae0b649
Iuri Calaras <silasbufu@yahoo.com>
2016-06-08 16:36:25 +0300

    first commit
    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name activeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the activeApp
+ */
+angular.module('activeApp')
+  .controller('MainCtrl', function ($scope) {
+    
+	function init(){
+		$scope.selectedActivity = null;
+	}
+    
+});
